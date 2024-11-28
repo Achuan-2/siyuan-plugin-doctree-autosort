1375643ba98ad3e5103d4f4f9d68b0f3037452fb
Tron <achuan-2@outlook.com>
2024-11-28 22:24:08 +0800

    🎉 first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Custom Sort Automation
+
+## Background
+
+If Siyuan Notes' document tree is sorted by document name, it lacks the freedom and flexibility I desire. Therefore, I usually opt for custom sorting. However, in custom sorting mode, manually dragging each document to sort by file name can be quite cumbersome. It would be great to have a sorting feature on top of custom sorting. This would allow me to create documents in any order, focusing on creation, and then sort them by document name later to achieve an organized note hierarchy.
+
+## Features
+
+When the document tree sorting mode is set to custom sorting, the plugin will add a sorting button to the right-click menu of parent documents and notebooks in the document tree for sorting. Currently, only sequential sorting is supported.
+
+## Powered by Love
+
+I am a poor graduate student currently studying. If you like my plugin, please consider giving a star to the GitHub repository and making a donation. This will motivate me to continue improving this plugin and developing new ones.
+
+![](https://fastly.jsdelivr.net/gh/Achuan-2/PicBed/assets/20241128221208-2024-11-28.png)
---
+++ src/index.ts
@@ -0,0 +1,109 @@
+import {
+    Plugin,
+    fetchPost,
+    IModel,
+} from "siyuan";
+import "@/index.scss";
+import { putFile, getHPathByID, getDoc, listDocTree, getFile } from "./api";
+
+export default class PluginSample extends Plugin {
+
+    customTab: () => IModel;
+
+    async onload() {
+        // 监听文档树右键菜单
+        this.eventBus.on("open-menu-doctree", this.addSortButton.bind(this));
+    }
+
+    private addSortButton = ({ detail }: any) => {
+        // 判断是否是自定义排序模式，不是则不添加按钮
+        if (window.siyuan.config.fileTree.sort != 6) {
+            return false;
+        }
+        const elements = detail.elements;
+        if (elements.length === 1) {
+            const element = elements[0];
+            const isParentDoc = element.getAttribute("data-count") > 0;
+            const isNotebook = element.getAttribute("data-type") === "navigation-root";
+            const id = isNotebook ? element.parentNode.getAttribute("data-url") : element.getAttribute("data-node-id");
+            // console.log(element);
+            if (isParentDoc || isNotebook) {
+                detail.menu.addItem({
+                    icon: "iconSort",
+                    label: this.i18n.sortMenu,
+                    click: () => this.sortDocuments(id, isNotebook)
+                });
+            }
+        }
+    }
+
+    private async sortDocuments(id: string, isNotebook: boolean) {
+
+        // 保存文件的辅助函数
+        let listDocTreeQuery;
+        let boxID;
+        if (!isNotebook) {// 子文档
+
+            // 获取父文档信息
+            const parentDocInfo = await getDoc(id);
+            boxID = parentDocInfo.box;
+            // 获取子文档列表
+            listDocTreeQuery = {
+                "notebook": boxID,
+                "path": parentDocInfo.path.replace(".sy", "")
+            };
+        } else {// 笔记本
+            boxID = id;
+            listDocTreeQuery = {
+                "notebook": boxID,
+                "path": "/"
+            };
+        }
+        // 获取子文档id
+        const childDocIds = (await listDocTree(boxID,listDocTreeQuery.path)).tree;
+        // console.log(childDocIds)
+        // 获取所有文档名称并排序
+        const idNamePairs = await Promise.all(childDocIds.map(async (doc: any) => {
+            const name = await getHPathByID(doc.id);
+            return { id: doc.id, name: name };
+        }));
+        idNamePairs.sort((a, b) => a.name.localeCompare(b.name));
+        console.log(idNamePairs)
+        // 创建排序结果对象
+        const sortedResult = {};
+        idNamePairs.forEach((pair, index) => {
+            sortedResult[pair.id] = index;
+        });
+        // 获取现有的sort.json文件
+        const sortJson = await getFile(`/data/${boxID}/.siyuan/sort.json`);
+        console.log(sortJson)
+
+
+        // 更新排序值
+        for (let id in sortedResult) {
+            if (sortJson.hasOwnProperty(id)) {
+                sortJson[id] = sortedResult[id];
+            }
+        }
+
+        // 保存更新后的sort.json文件
+        await putFile(`/data/${boxID}/.siyuan/sort.json`, sortJson);
+
+        // 刷新文档树
+        let element;
+        if (!isNotebook) {
+            element = document.querySelector(`.file-tree li[data-node-id="${id}"] > .b3-list-item__toggle--hl`);
+            if (element) {
+                element.click();
+                element.click();
+            }
+        } else {
+            element = document.querySelector(`.file-tree ul[data-url="${id}"] > li >.b3-list-item__toggle--hl`);
+            if (element) {
+                element.click();
+                element.click();
+            }
+        }
+
+    }
+}
